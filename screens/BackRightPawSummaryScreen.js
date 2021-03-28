a256d270d9df128c5e4d43196795baceba17c6c1
Pawel Bak <pawelbak300589@gmail.com>
2021-03-28 18:46:08 +0100

    Update back right paw screens

--- screens/BackRightPawSummaryScreen.js
+++ screens/BackRightPawSummaryScreen.js
@@ -1,29 +1,57 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { StyleSheet, View, ScrollView } from 'react-native';
+import { useSelector } from 'react-redux';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
-import Colors from '../constants/Colors';
+import HeaderButton from '../components/HeaderButton';
+import SummaryRow from '../components/SummaryRow';
 
 const BackRightPawSummaryScreen = (props) => {
+  const backRightPawData = useSelector(state => state.session.backRightPaw);
+
   return (
-    <View style={styles.screen}>
-      <Ionicons name="paw" size={180} color={Colors.redColor} />
-      <Text>BackRightPawSummaryScreen</Text>
-    </View>
+    <ScrollView style={styles.screen}>
+      <View style={styles.summaryList}>
+        <SummaryRow clawText='CLAW 1' status={backRightPawData.firstClaw} />
+        <SummaryRow clawText='CLAW 2' status={backRightPawData.secondClaw} />
+        <SummaryRow clawText='CLAW 3' status={backRightPawData.thirdClaw} />
+        <SummaryRow clawText='CLAW 4' status={backRightPawData.fourthClaw} />
+      </View>
+    </ScrollView>
   );
 };
 
 BackRightPawSummaryScreen.navigationOptions = (navData) => {
   return {
-    headerTitle: 'Back Right Paw Summary'
+    headerTitle: 'BRP - Summary',
+    headerRight: () => (<HeaderButtons HeaderButtonComponent={HeaderButton}>
+      <Item title="Finish paw" iconName='checkmark' onPress={() => {
+        navData.navigation.navigate({ routeName: 'FrontLeftPaw' });
+      }} />
+    </HeaderButtons>)
   };
 };
 
 const styles = StyleSheet.create({
   screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+  },
+  summaryList: {
+    padding: 5,
+  },
+  summaryRow: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    margin: 5,
+  },
+  listItemsContainer: {
+    paddingBottom: 5,
+    paddingTop: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: '#bbb'
+  },
+  itemText: {
+    marginLeft: 10
   }
 });
 
