41e2551243d86d2aab2796fe991ecaef3f428d71
Pawel Bak <pawelbak300589@gmail.com>
2021-05-17 19:04:54 +0100

    Update new session navigator to react-navigation version 5

--- navigation/NewSessionNavigator.js
+++ navigation/NewSessionNavigator.js
@@ -1,13 +1,18 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import SessionNavigator from './SessionNavigator';
+import PawsTabsNavigator from './PawsTabsNavigator';
 import PreNewSessionNavigator from './PreNewSessionNavigator';
 import PostNewSessionNavigator from './PostNewSessionNavigator';
 
-const NewSessionNavigator = createSwitchNavigator({
-  PreNewSession: PreNewSessionNavigator,
-  Session: SessionNavigator,
-  PostNewSession: PostNewSessionNavigator,
-}, {});
+const NewSessionStack = createStackNavigator();
 
-export default createAppContainer(NewSessionNavigator);
+const NewSessionNavigator = () => {
+  return <NewSessionStack.Navigator screenOptions={DefaultStackNavOptions}>
+    <NewSessionStack.Screen name="PreNewSession" component={PreNewSessionNavigator} />
+    <NewSessionStack.Screen name="Paws" component={PawsTabsNavigator} />
+    <NewSessionStack.Screen name="PostNewSession" component={PostNewSessionNavigator} />
+  </NewSessionStack.Navigator>
+};
+
+export default NewSessionNavigator;
