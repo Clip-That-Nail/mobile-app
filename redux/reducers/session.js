0b3359941f43e59fa44351f6cbab860df79f019a
Pawel Bak <pawelbak300589@gmail.com>
2021-05-23 15:39:23 +0100

    Update session reducer to have more data for each claw

--- redux/reducers/session.js
+++ redux/reducers/session.js
@@ -4,75 +4,223 @@ const initialState = {
   frontLeftPaw: {
     complete: false,
     claws: {
-      firstClaw: 'unchecked',
-      secondClaw: 'unchecked',
-      thirdClaw: 'unchecked',
-      fourthClaw: 'unchecked',
-      dewClaw: 'unchecked',
+      firstClaw: {
+        status: 'unchecked',
+        outcome: '',
+        behavior: ''
+      },
+      secondClaw: {
+        status: 'unchecked',
+        outcome: '',
+        behavior: ''
+      },
+      thirdClaw: {
+        status: 'unchecked',
+        outcome: '',
+        behavior: ''
+      },
+      fourthClaw: {
+        status: 'unchecked',
+        outcome: '',
+        behavior: ''
+      },
+      dewClaw: {
+        status: 'unchecked',
+        outcome: '',
+        behavior: ''
+      },
     }
   },
   frontRightPaw: {
     complete: false,
     claws: {
-      firstClaw: 'unchecked',
-      secondClaw: 'unchecked',
-      thirdClaw: 'unchecked',
-      fourthClaw: 'unchecked',
-      dewClaw: 'unchecked',
+      firstClaw: {
+        status: 'unchecked',
+        outcome: '',
+        behavior: ''
+      },
+      secondClaw: {
+        status: 'unchecked',
+        outcome: '',
+        behavior: ''
+      },
+      thirdClaw: {
+        status: 'unchecked',
+        outcome: '',
+        behavior: ''
+      },
+      fourthClaw: {
+        status: 'unchecked',
+        outcome: '',
+        behavior: ''
+      },
+      dewClaw: {
+        status: 'unchecked',
+        outcome: '',
+        behavior: ''
+      },
     }
   },
   backLeftPaw: {
     complete: false,
     claws: {
-      firstClaw: 'unchecked',
-      secondClaw: 'unchecked',
-      thirdClaw: 'unchecked',
-      fourthClaw: 'unchecked',
+      firstClaw: {
+        status: 'unchecked',
+        outcome: '',
+        behavior: ''
+      },
+      secondClaw: {
+        status: 'unchecked',
+        outcome: '',
+        behavior: ''
+      },
+      thirdClaw: {
+        status: 'unchecked',
+        outcome: '',
+        behavior: ''
+      },
+      fourthClaw: {
+        status: 'unchecked',
+        outcome: '',
+        behavior: ''
+      }
     }
   },
   backRightPaw: {
     complete: false,
     claws: {
-      firstClaw: 'unchecked',
-      secondClaw: 'unchecked',
-      thirdClaw: 'unchecked',
-      fourthClaw: 'unchecked',
+      firstClaw: {
+        status: 'unchecked',
+        outcome: '',
+        behavior: ''
+      },
+      secondClaw: {
+        status: 'unchecked',
+        outcome: '',
+        behavior: ''
+      },
+      thirdClaw: {
+        status: 'unchecked',
+        outcome: '',
+        behavior: ''
+      },
+      fourthClaw: {
+        status: 'unchecked',
+        outcome: '',
+        behavior: ''
+      }
     }
   },
 }
 
 const sessionReducer = (state = initialState, action) => {
   switch (action.type) {
-    case sessionTypes.UPDATE_FRONT_LEFT_PAW:
+    case sessionTypes.UPDATE_FRONT_LEFT_PAW_STATUS:
       return {
         ...state,
         frontLeftPaw: {
           ...state.frontLeftPaw,
-          claws: action.pawData
+          claws: {
+            firstClaw: {
+              ...state.frontLeftPaw.claws.firstClaw,
+              status: action.pawData.firstClaw,
+            },
+            secondClaw: {
+              ...state.frontLeftPaw.claws.secondClaw,
+              status: action.pawData.secondClaw,
+            },
+            thirdClaw: {
+              ...state.frontLeftPaw.claws.thirdClaw,
+              status: action.pawData.thirdClaw,
+            },
+            fourthClaw: {
+              ...state.frontLeftPaw.claws.fourthClaw,
+              status: action.pawData.fourthClaw,
+            },
+            dewClaw: {
+              ...state.frontLeftPaw.claws.dewClaw,
+              status: action.pawData.dewClaw,
+            },
+          }
         }
       };
-    case sessionTypes.UPDATE_FRONT_RIGHT_PAW:
+    case sessionTypes.UPDATE_FRONT_RIGHT_PAW_STATUS:
       return {
         ...state,
         frontRightPaw: {
           ...state.frontRightPaw,
-          claws: action.pawData
+          claws: {
+            firstClaw: {
+              ...state.frontRightPaw.claws.firstClaw,
+              status: action.pawData.firstClaw,
+            },
+            secondClaw: {
+              ...state.frontRightPaw.claws.secondClaw,
+              status: action.pawData.secondClaw,
+            },
+            thirdClaw: {
+              ...state.frontRightPaw.claws.thirdClaw,
+              status: action.pawData.thirdClaw,
+            },
+            fourthClaw: {
+              ...state.frontRightPaw.claws.fourthClaw,
+              status: action.pawData.fourthClaw,
+            },
+            dewClaw: {
+              ...state.frontRightPaw.claws.dewClaw,
+              status: action.pawData.dewClaw,
+            },
+          }
         }
       };
-    case sessionTypes.UPDATE_BACK_LEFT_PAW:
+    case sessionTypes.UPDATE_BACK_LEFT_PAW_STATUS:
       return {
         ...state,
         backLeftPaw: {
           ...state.backLeftPaw,
-          claws: action.pawData
+          claws: {
+            firstClaw: {
+              ...state.backLeftPaw.claws.firstClaw,
+              status: action.pawData.firstClaw,
+            },
+            secondClaw: {
+              ...state.backLeftPaw.claws.secondClaw,
+              status: action.pawData.secondClaw,
+            },
+            thirdClaw: {
+              ...state.backLeftPaw.claws.thirdClaw,
+              status: action.pawData.thirdClaw,
+            },
+            fourthClaw: {
+              ...state.backLeftPaw.claws.fourthClaw,
+              status: action.pawData.fourthClaw,
+            }
+          }
         }
       };
-    case sessionTypes.UPDATE_BACK_RIGHT_PAW:
+    case sessionTypes.UPDATE_BACK_RIGHT_PAW_STATUS:
       return {
         ...state,
         backRightPaw: {
           ...state.backRightPaw,
-          claws: action.pawData
+          claws: {
+            firstClaw: {
+              ...state.backRightPaw.claws.firstClaw,
+              status: action.pawData.firstClaw,
+            },
+            secondClaw: {
+              ...state.backRightPaw.claws.secondClaw,
+              status: action.pawData.secondClaw,
+            },
+            thirdClaw: {
+              ...state.backRightPaw.claws.thirdClaw,
+              status: action.pawData.thirdClaw,
+            },
+            fourthClaw: {
+              ...state.backRightPaw.claws.fourthClaw,
+              status: action.pawData.fourthClaw,
+            }
+          }
         }
       };
     case sessionTypes.COMPLETE_FRONT_LEFT_PAW:
