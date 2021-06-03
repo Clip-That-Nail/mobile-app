5180632252d9a865f9b1b695ba306f9ab1f4a008
Pawel Bak <pawelbak300589@gmail.com>
2021-06-03 22:14:41 +0100

    Add thunk middleware to the store

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,11 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import sessionReducer from './reducers/session';
+import ReduxThunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
   session: sessionReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default store;
