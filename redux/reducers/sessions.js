5a2c33b4b664fd18fd1f52384efd1aea8d6012b7
Pawel Bak <pawelbak300589@gmail.com>
2021-09-01 19:58:39 +0100

    Add sessions redux reducer

---
+++ redux/reducers/sessions.js
@@ -0,0 +1,30 @@
+import sessionsTypes from '../types/sessions';
+import Session from '../../models/session';
+
+const initialState = {
+  sessions: []
+}
+
+const sessionReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case sessionsTypes.SET_SESSIONS:
+      return {
+        sessions: action.sessions.map(session => new Session(session.id.toString(), dog.frontLeft, dog.frontRight, dog.backLeft, dog.backRight))
+      }
+    case sessionsTypes.ADD_SESSION:
+      const newSession = new Session(
+        action.sessionData.id.toString(),
+        action.sessionData.paws.frontLeft,
+        action.sessionData.paws.frontRight,
+        action.sessionData.paws.backLeft,
+        action.sessionData.paws.backRight,
+      );
+      return {
+        sessions: state.sessions.concat(newSession)
+      }
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import sessionReducer from './reducers/session';
+import sessionsReducer from './reducers/sessions';
 import dogsReducer from './reducers/dogs';
 import ReduxThunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
   session: sessionReducer,
+  sessions: sessionsReducer,
   dogs: dogsReducer,
 });
 
