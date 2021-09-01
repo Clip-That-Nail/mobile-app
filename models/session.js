d7a6fae6d85a5df544ae29c671b6f4c4b5d35871
Pawel Bak <pawelbak300589@gmail.com>
2021-09-01 19:55:34 +0100

    Add session model

---
+++ models/session.js
@@ -0,0 +1,11 @@
+class Session {
+  constructor(id, frontLeft, frontRight, backLeft, backRight) {
+    this.id = id;
+    this.frontLeft = frontLeft;
+    this.frontRight = frontRight;
+    this.backLeft = backLeft;
+    this.backRight = backRight;
+  }
+}
+
+export default Session;
