3207ec831ce2fa6a5d821642d4bfe8f9ebfee277
Pawel Bak <pawelbak300589@gmail.com>
2021-09-13 21:41:20 +0100

    Display simple list of sessions (just json data for now - CHECKPOINT)

--- screens/Sessions/SessionsListScreen.js
+++ screens/Sessions/SessionsListScreen.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
+
+import { loadSessions } from '../../redux/actions/sessions';
 
 const SessionsListScreen = (props) => {
+  const dispatch = useDispatch();
+  const sessions = useSelector(state => state.sessions.sessions);
+
+  // console.log('sessions', sessions);
+
+  useEffect(() => {
+    dispatch(loadSessions());
+  }, [dispatch]);
+
   return (
-    <View><Text>Sessions List Screen</Text></View>
+    <View>
+      <FlatList
+        data={sessions}
+        keyExtractor={item => item.id}
+        renderItem={itemData => <Text>Session: {JSON.stringify(itemData.item)}</Text>}
+      />
+    </View>
   );
 };
 
