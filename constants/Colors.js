546792d2b25f081469c86f979f0e37eb2e460ee9
Pawel Bak <pawelbak300589@gmail.com>
2021-09-19 19:53:26 +0100

    Add more colours

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,9 +1,23 @@
 // https://www.color-hex.com/color-palette/97787
 
 export default {
+  redLightColor: '#ffdcdc',
   redColor: '#ff6666',
+  redDarkColor: '#a10000',
+
+  yellowLightColor: '#fff3dc',
   yellowColor: '#ffc966',
+  yellowDarkColor: '#a16800',
+
+  greenLightColor: '#e8f4e8',
   greenColor: '#66B266',
+  greenDarkColor: '#366c36',
+
+  blueLightColor: '#e4f0f8',
   blueColor: '#66a9d9',
+  blueDarkColor: '#205982',
+
+  violetLightColor: '#f3eef7',
   violetColor: '#a982c6',
+  violetDarkColor: '#5a3675',
 }
