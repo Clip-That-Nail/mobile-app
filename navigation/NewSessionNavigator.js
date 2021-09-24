d8d51d33f878afa318eb74c6345b88d15d9cb069
Pawel Bak <pawelbak300589@gmail.com>
2021-09-24 17:59:19 +0100

    Remove pre new session screen and navigator

--- navigation/NewSessionNavigator.js
+++ navigation/NewSessionNavigator.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import PawsTabsNavigator from './PawsTabsNavigator';
-import PreNewSessionNavigator from './PreNewSessionNavigator';
 import PostNewSessionNavigator from './PostNewSessionNavigator';
 
 import Colors from '../constants/Colors';
@@ -11,7 +10,6 @@ const NewSessionStack = createStackNavigator();
 
 const NewSessionNavigator = () => {
   return <NewSessionStack.Navigator screenOptions={{ headerShown: false, headerBackground: Colors.greenColor }}>
-    <NewSessionStack.Screen name="PreNewSession" component={PreNewSessionNavigator} />
     <NewSessionStack.Screen name="Paws" component={PawsTabsNavigator} />
     <NewSessionStack.Screen name="PostNewSession" component={PostNewSessionNavigator} />
   </NewSessionStack.Navigator>
