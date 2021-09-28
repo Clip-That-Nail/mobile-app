197fe26ed3e1751461b1b86a8b489a2d48355238
Pawel Bak <pawelbak300589@gmail.com>
2021-09-28 19:47:08 +0100

    Add FINISH_SESSION case to the sessions reducers

--- redux/reducers/sessions.js
+++ redux/reducers/sessions.js
@@ -1,4 +1,5 @@
 import sessionsTypes from '../types/sessions';
+import newSessionsTypes from '../types/newSession';
 import Session from '../../models/session';
 
 const initialState = {
@@ -11,11 +12,12 @@ const sessionReducer = (state = initialState, action) => {
       return {
         sessions: action.sessions.map(session => new Session(session.id, session.status, session.createDate, session.pet, session.frontLeft, session.frontRight, session.backLeft, session.backRight))
       }
-    case sessionsTypes.ADD_SESSION:
+    case newSessionsTypes.FINISH_SESSION:
       const newSession = new Session(
-        action.sessionData.id.toString(),
+        action.sessionData.id,
         action.sessionData.status,
         Date.now(),
+        action.sessionData.pet,
         action.sessionData.paws.frontLeft,
         action.sessionData.paws.frontRight,
         action.sessionData.paws.backLeft,
@@ -24,6 +26,19 @@ const sessionReducer = (state = initialState, action) => {
       return {
         sessions: state.sessions.concat(newSession)
       }
+    // case sessionsTypes.ADD_SESSION:
+    //   const newSession = new Session(
+    //     action.sessionData.id.toString(),
+    //     action.sessionData.status,
+    //     Date.now(),
+    //     action.sessionData.paws.frontLeft,
+    //     action.sessionData.paws.frontRight,
+    //     action.sessionData.paws.backLeft,
+    //     action.sessionData.paws.backRight,
+    //   );
+    //   return {
+    //     sessions: state.sessions.concat(newSession)
+    //   }
     default:
       return state;
   }
