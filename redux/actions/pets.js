a4a82a7230c4018259aeaefc8b2947c72a3c01c1
Pawel Bak <pawelbak300589@gmail.com>
2021-10-12 19:55:45 +0100

    Add updatePet action creator

--- redux/actions/pets.js
+++ redux/actions/pets.js
@@ -1,7 +1,7 @@
 import * as FileSystem from 'expo-file-system';
 
 import petsTypes from '../types/pets';
-import { fetchPets, insertPet } from '../../helpers/db';
+import { fetchPets, insertPet, updatePet as updatePetInDB } from '../../helpers/db';
 
 export const loadPets = () => {
   return async (dispatch) => {
@@ -32,6 +32,28 @@ export const addPet = (name, type, breed, image) => {
       dispatch({ type: petsTypes.ADD_DOG, petData: { id: dbResult.insertId, name, type, breed, image: newPath }});
     } catch (err) {
       console.log('addPet error:', err);
+      throw err;
+    }
+  }
+};
+
+export const updatePet = (petId, name, type, breed, image) => {
+  return async (dispatch) => {
+    const fileName = image.split('/').pop();
+    const newPath = FileSystem.documentDirectory + fileName;
+
+    try {
+      await FileSystem.moveAsync({
+        from: image,
+        to: newPath
+      });
+      const dbResult = await updatePetInDB(petId, name, type, breed, newPath);
+      console.log('updatePet result:', dbResult);
+      console.log('action type : ', petsTypes.UPDATE_DOG);
+
+      dispatch({ type: petsTypes.UPDATE_DOG, petData: { id: dbResult.insertId, name, type, breed, image: newPath }});
+    } catch (err) {
+      console.log('updatePet error:', err);
       throw err;
     }
   }
