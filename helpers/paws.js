a8921e7bb3ecf226a0d365e6e7dbe3227ea42447
Pawel Bak <pawelbak300589@gmail.com>
2021-10-21 22:18:43 +0100

    Add paws data helper to store names etc

---
+++ helpers/paws.js
@@ -0,0 +1,120 @@
+export const pawsData = {
+  frontLeft: {
+    id: 'frontLeft',
+    name: 'Front Left Paw',
+    shortName: 'FL',
+    claws: {
+      firstClaw: {
+        id: 'first',
+        name: 'First Claw',
+        shortName: '1',
+      },
+      secondClaw: {
+        id: 'second',
+        name: 'Second Claw',
+        shortName: '2',
+      },
+      thirdClaw: {
+        id: 'third',
+        name: 'Third Claw',
+        shortName: '3',
+      },
+      fourthClaw: {
+        id: 'fourth',
+        name: 'Fourth Claw',
+        shortName: '4',
+      },
+      dewClaw: {
+        id: 'dew',
+        name: 'Dew Claw',
+        shortName: 'D',
+      },
+    }
+  },
+  frontRight: {
+    id: 'frontRight',
+    name: 'Front Right Paw',
+    shortName: 'FR',
+    claws: {
+      firstClaw: {
+        id: 'first',
+        name: 'First Claw',
+        shortName: '1',
+      },
+      secondClaw: {
+        id: 'second',
+        name: 'Second Claw',
+        shortName: '2',
+      },
+      thirdClaw: {
+        id: 'third',
+        name: 'Third Claw',
+        shortName: '3',
+      },
+      fourthClaw: {
+        id: 'fourth',
+        name: 'Fourth Claw',
+        shortName: '4',
+      },
+      dewClaw: {
+        id: 'dew',
+        name: 'Dew Claw',
+        shortName: 'D',
+      },
+    }
+  },
+  backLeft: {
+    id: 'backLeft',
+    name: 'Back Left Paw',
+    shortName: 'BL',
+    claws: {
+      firstClaw: {
+        id: 'first',
+        name: 'First Claw',
+        shortName: '1',
+      },
+      secondClaw: {
+        id: 'second',
+        name: 'Second Claw',
+        shortName: '2',
+      },
+      thirdClaw: {
+        id: 'third',
+        name: 'Third Claw',
+        shortName: '3',
+      },
+      fourthClaw: {
+        id: 'fourth',
+        name: 'Fourth Claw',
+        shortName: '4',
+      },
+    }
+  },
+  backRight: {
+    id: 'backRight',
+    name: 'Back Right Paw',
+    shortName: 'BR',
+    claws: {
+      firstClaw: {
+        id: 'first',
+        name: 'First Claw',
+        shortName: '1',
+      },
+      secondClaw: {
+        id: 'second',
+        name: 'Second Claw',
+        shortName: '2',
+      },
+      thirdClaw: {
+        id: 'third',
+        name: 'Third Claw',
+        shortName: '3',
+      },
+      fourthClaw: {
+        id: 'fourth',
+        name: 'Fourth Claw',
+        shortName: '4',
+      },
+    }
+  }
+};
