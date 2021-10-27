da4e31a8d61faebac83f6b360ab53a9cfe07e8f0
Pawel Bak <pawelbak300589@gmail.com>
2021-10-27 22:17:17 +0100

    Add pet disabilities section to the pet detail screen

--- screens/Pets/PetDetailScreen.js
+++ screens/Pets/PetDetailScreen.js
@@ -10,8 +10,60 @@ const PetDetailScreen = (props) => {
   const petId = props.route.params.petId;
   const selectedPet = useSelector(state => state.pets.pets.find(pet => pet.id === petId));
 
+  const displayPetDisabilities = () => {
+    if (selectedPet.disabilities.length > 0) {
+      const frontLeftDisability = [];
+      const frontRightDisability = [];
+      const backLeftDisability = [];
+      const backRightDisability = [];
+
+      selectedPet.disabilities.map(disability => {
+        switch (disability.paw) {
+          case 'frontLeft':
+            frontLeftDisability.push(disability);
+            break;
+          case 'frontRight':
+            frontRightDisability.push(disability);
+            break;
+          case 'backLeft':
+            backLeftDisability.push(disability);
+            break;
+          case 'backRight':
+            backRightDisability.push(disability);
+            break;
+        }
+      });
+
+      // console.log('frontLeftDisability', frontLeftDisability);
+      // console.log('frontRightDisability', frontRightDisability);
+      // console.log('backLeftDisability', backLeftDisability);
+      // console.log('backRightDisability', backRightDisability);
+
+      return (
+        <View style={styles.disabilities}>
+          <View style={styles.pawsRow}>
+            <Card style={styles.disabledPaw}>
+              <PawImage pawName="frontLeft" pawData={frontLeftDisability} />
+            </Card>
+            <Card style={styles.disabledPaw}>
+              <PawImage pawName="frontRight" pawData={frontRightDisability} />
+            </Card>
+          </View>
+          <View style={styles.pawsRow}>
+            <Card style={styles.disabledPaw}>
+              <PawImage pawName="backLeft" pawData={backLeftDisability} />
+            </Card>
+            <Card style={styles.disabledPaw}>
+              <PawImage pawName="backRight" pawData={backRightDisability} />
+            </Card>
+          </View>
+        </View>
+      )
+    }
+  }
+
   return (
-    <ScrollView contentContainerStyle={{ alignItems: 'center' }}>
+    <ScrollView contentContainerStyle={{}}>
       <Image source={{ uri: selectedPet.imageUri }} style={styles.image} />
       <Card style={styles.card}>
         <View>
@@ -19,15 +71,7 @@ const PetDetailScreen = (props) => {
         </View>
       </Card>
       {
-        selectedPet.disabled && (
-          <Card style={styles.card}>
-            <Text>{selectedPet.disabled}</Text>
-            {/* <PawImage pawData={} />
-            <PawImage pawData={} />
-            <PawImage pawData={} />
-            <PawImage pawData={} /> */}
-          </Card>
-        )
+        selectedPet.disabled && displayPetDisabilities()
       }
     </ScrollView>
   );
@@ -47,11 +91,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#ccc'
   },
   card: {
-    width: '95%',
     marginHorizontal: 10,
     marginTop: 10,
     marginBottom: 0
-  }
+  },
+  disabilities: {
+  },
+  pawsRow: {
+    flexDirection: 'row',
+  },
+  disabledPaw: {
+    flex: 1,
+  },
 });
 
 export default PetDetailScreen;
