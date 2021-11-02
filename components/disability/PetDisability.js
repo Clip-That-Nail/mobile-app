ecf4a69c870a404d420715829db99f5c9304e500
Pawel Bak <pawelbak300589@gmail.com>
2021-11-02 19:57:02 +0000

    Simplify pet disability component

--- components/disability/PetDisability.js
+++ components/disability/PetDisability.js
@@ -1,47 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 
 import Card from '../Card';
 import PawImage from '../PawImage';
 import BoxTitle from '../titles/BoxTitle';
 
-const PetDisability = ({ disabilities }) => {
-  const [frontLeftDisability, setFrontLeftDisability] = useState([]);
-  const [frontRightDisability, setFrontRightDisability] = useState([]);
-  const [backLeftDisability, setBackLeftDisability] = useState([]);
-  const [backRightDisability, setBackRightDisability] = useState([]);
-
-  useEffect(() => {
-    if (disabilities.length > 0) {
-      setFrontLeftDisability(disabilities.filter(disability => disability.paw === 'frontLeft'));
-      setFrontRightDisability(disabilities.filter(disability => disability.paw === 'frontRight'));
-      setBackLeftDisability(disabilities.filter(disability => disability.paw === 'backLeft'));
-      setBackRightDisability(disabilities.filter(disability => disability.paw === 'backRight'));
-    }
-  }, [disabilities, setFrontLeftDisability, setFrontRightDisability, setBackLeftDisability, setBackRightDisability]);
-
-  return (
-    <Card style={styles.disabilities}>
-      <BoxTitle title="Disabled Claws" />
-      <View style={styles.pawsRow}>
-        <View style={styles.disabledPaw}>
-          <PawImage pawName="frontLeft" pawData={frontLeftDisability} />
-        </View>
-        <View style={styles.disabledPaw}>
-          <PawImage pawName="frontRight" pawData={frontRightDisability} />
-        </View>
+const PetDisability = ({ disabilities }) => (
+  <Card style={styles.disabilities}>
+    <BoxTitle title="Disabled Claws" />
+    <View style={styles.pawsRow}>
+      <View style={styles.disabledPaw}>
+        <PawImage pawName="frontLeft" pawData={disabilities.frontLeft} />
       </View>
-      <View style={styles.pawsRow}>
-        <View style={styles.disabledPaw}>
-          <PawImage pawName="backLeft" pawData={backLeftDisability} />
-        </View>
-        <View style={styles.disabledPaw}>
-          <PawImage pawName="backRight" pawData={backRightDisability} />
-        </View>
+      <View style={styles.disabledPaw}>
+        <PawImage pawName="frontRight" pawData={disabilities.frontRight} />
       </View>
-    </Card>
-  );
-};
+    </View>
+    <View style={styles.pawsRow}>
+      <View style={styles.disabledPaw}>
+        <PawImage pawName="backLeft" pawData={disabilities.backLeft} />
+      </View>
+      <View style={styles.disabledPaw}>
+        <PawImage pawName="backRight" pawData={disabilities.backRight} />
+      </View>
+    </View>
+  </Card>
+);
 
 const styles = StyleSheet.create({
   disabilities: {
