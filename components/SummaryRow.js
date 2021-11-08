2af709047ff0d28c2c3bf3b409a1dfe27cb31e8f
Pawel Bak <pawelbak300589@gmail.com>
2021-11-08 21:02:04 +0000

    Add outcome and behaviour images to the summary row

--- components/SummaryRow.js
+++ components/SummaryRow.js
@@ -3,6 +3,8 @@ import { StyleSheet, View, Text } from 'react-native';
 
 import SummarySpecialIcon from './SummarySpecialIcon';
 import SummaryPicker from '../components/SummaryPicker';
+import OutcomeImage from '../components/OutcomeImage';
+import BehaviourImage from '../components/BehaviourImage';
 
 const SummaryRow = (props) => {
   const { claw, onOutcomeChange, onBehaviourChange } = props;
@@ -17,10 +19,20 @@ const SummaryRow = (props) => {
 
   return (
     <View style={styles.listItemsContainer}>
-      <View style={{ ...styles.summaryRow, justifyContent: 'flex-start' }}>
-        <SummarySpecialIcon status={claw.status} />
-        <View style={styles.itemText}>
-          <Text>{claw.text}</Text>
+      <View style={{ ...styles.summaryRow, justifyContent: 'space-between' }}>
+        <View style={{ ...styles.summaryRow }}>
+          <SummarySpecialIcon status={claw.status} />
+          <View style={styles.itemText}>
+            <Text>{claw.text}</Text>
+          </View>
+        </View>
+        <View style={{ ...styles.summaryRow, justifyContent: 'center' }}>
+          <View style={{ marginLeft: 5 }}>
+            <OutcomeImage size={26} status={claw.status} outcome={claw.outcome} />
+          </View>
+          <View style={{ marginLeft: 5 }}>
+            <BehaviourImage size={26} behaviour={claw.behaviour} />
+          </View>
         </View>
       </View>
       <View style={styles.summaryRow}>
