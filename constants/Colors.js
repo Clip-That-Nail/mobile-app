ae4b7e598222b48e1f53532bd1256e8f88482a36
Pawel Bak <pawelbak300589@gmail.com>
2021-11-15 19:20:48 +0000

    Add more colours

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,6 +1,20 @@
 // https://www.color-hex.com/color-palette/97787
 
 export default {
+  black: '#000000',
+  white: '#FFFFFF',
+
+  gray000: '#f6f6f6',
+  gray100: '#e2e2e2',
+  gray200: '#c5c5c5',
+  gray300: '#a7a7a7',
+  gray400: '#949494',
+  gray500: '#808080',
+  gray600: '#6c6c6c',
+  gray700: '#4f4f4f',
+  gray800: '#323232',
+  gray900: '#141414',
+
   redLightColor: '#ffdcdc',
   redColor: '#ff6666',
   redDarkColor: '#a10000',
