d0dddf4b41dc8fc4800163eb0a27f264db3ab43a
Pawel Bak <pawelbak300589@gmail.com>
2021-11-15 19:34:14 +0000

    Update OutcomesAndBehaviours to change text colors etc

--- components/OutcomesAndBehaviours.js
+++ components/OutcomesAndBehaviours.js
@@ -13,19 +13,21 @@ const OutcomesAndBehaviours = ({ pawData, statuses, outcomes, behaviours }) => {
   // console.log('behaviours', behaviours);
 
   const clawColor = {
-    disabled: '#444',
-    checked: Colors.greenColor,
-    bleeded: Colors.redColor,
-    warning: Colors.yellowColor,
+    disabled: { bg: '#999999', text: Colors.white },
+    checked: { bg: Colors.greenColor, text: Colors.white },
+    bleeded: { bg: Colors.redColor, text: Colors.white },
+    warning: { bg: Colors.yellowColor, text: Colors.black },
   };
 
   return (
     <View style={styles.outcomesAndBehaviours}>
       {
         Object.keys(pawData.claws).map((clawData) => (
-          <View style={styles.clawRow}>
-            <View style={{ ...styles.clawContainer, backgroundColor: clawColor[statuses[pawData.claws[clawData].id]] }}>
-              <Text style={styles.clawText}>{pawData.claws[clawData].shortName}</Text>
+          <View style={styles.clawRow} key={pawData.claws[clawData].id}>
+            <View style={{ ...styles.clawContainer, backgroundColor: clawColor[statuses[pawData.claws[clawData].id]]?.bg }}>
+              <Text style={{ ...styles.clawText, color: clawColor[statuses[pawData.claws[clawData].id]]?.text }}>
+                {pawData.claws[clawData].shortName}
+              </Text>
             </View>
             <View style={styles.imageContainer}>
               <OutcomeImage size={40} status={statuses[pawData.claws[clawData].id]} outcome={outcomes[pawData.claws[clawData].id]} />
@@ -47,11 +49,12 @@ const styles = StyleSheet.create({
     flexDirection: 'row'
   },
   clawContainer: {
-    width: 40,
-    height: 40,
+    width: 44,
+    height: 44,
     margin: 2,
     justifyContent: 'center',
     alignItems: 'center',
+    borderRadius: 5,
   },
   clawText: {
     fontSize: 20
